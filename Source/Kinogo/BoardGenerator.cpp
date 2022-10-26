8ee083be0a3274869c4a988062b09da3e5482d49
bbompk <panithiv@gmail.com>
2022-10-27 03:03:18 +0700

    gen board v1

---
+++ Source/Kinogo/BoardGenerator.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BoardGenerator.h"
+
+// Sets default values
+ABoardGenerator::ABoardGenerator()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABoardGenerator::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABoardGenerator::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
